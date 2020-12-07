aaf9925bb5bcf449d0523e2050a247ff9597cc24
Sina <sinaraoufi@outlook.com>
2020-12-07 18:20:25 +0330

    add Counting Sort implementation

---
+++ include/CountingSort.hpp
@@ -0,0 +1,8 @@
+#ifndef COUNTING_SORT_IG
+#define COUNTING_SORT_IG
+
+#include <iostream>
+
+void countingSort(int[], size_t);
+
+#endif
---
+++ src/CountingSort.cpp
@@ -0,0 +1,36 @@
+#include "CountingSort.hpp"
+
+using namespace std;
+
+void countingSort(int arr[], size_t arraySize)
+{
+    // Find maximum value in the array
+    int max = arr[0];
+    for (size_t i = 1; i < arraySize; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    // Create new array for counting values stored in the array
+    int countingArray[max + 1];
+    for (size_t i = 0; i < max + 1; i++)
+        countingArray[i] = 0;
+
+    // Count each value
+    for (size_t i = 0; i < arraySize; i++)
+        countingArray[arr[i]]++;
+
+    // Calculate cumulative sum of elements
+    for (size_t i = 1; i < max + 1; i++)
+        countingArray[i] += countingArray[i - 1];
+
+    int temp[arraySize];
+    for (size_t i = 0; i < arraySize; i++)
+    {
+        temp[countingArray[arr[i]] - 1] = arr[i];
+        countingArray[arr[i]]--;
+    }
+
+    // Copy all values from temp array to main array
+    for (size_t i = 0; i < arraySize; i++)
+        arr[i] = temp[i];
+}
