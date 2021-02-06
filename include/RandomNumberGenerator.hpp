7f9f43f6d4181032afa2ddf370f30efb984c4b9f
Sina Raoufi <sinaraoufi@outlook.com>
2021-02-06 20:31:51 +0330

    Revert "change return type of random number generator function"
    This reverts commit f24dbb624798fbbc8e1e7d17f98017460f5ab95c.

--- include/RandomNumberGenerator.hpp
+++ include/RandomNumberGenerator.hpp
@@ -1,7 +1,9 @@
 #ifndef RANDOM_NUMBER_GENERATOR_IG
 #define RANDOM_NUMBER_GENERATOR_IG
 
+typedef unsigned short int us;
+
 // function for generating random number
-int generateRandomNumber(int, int);
+us generateRandomNumber(us, us);
 
 #endif
