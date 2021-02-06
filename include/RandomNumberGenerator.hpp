84d85ce80923fc4a0204bafb67e38f73947796fb
Sina Raoufi <sinaraoufi@outlook.com>
2021-02-06 20:32:54 +0330

    change return type of random number generator function

--- include/RandomNumberGenerator.hpp
+++ include/RandomNumberGenerator.hpp
@@ -1,9 +1,7 @@
 #ifndef RANDOM_NUMBER_GENERATOR_IG
 #define RANDOM_NUMBER_GENERATOR_IG
 
-typedef unsigned short int us;
-
 // function for generating random number
-us generateRandomNumber(us, us);
+int generateRandomNumber(int, int);
 
 #endif
--- src/RandomNumberGenerator.cpp
+++ src/RandomNumberGenerator.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 // function for generating random number
-us generateRandomNumber(us range1, us range2)
+int generateRandomNumber(int range1, int range2)
 {
     random_device randomDevice;
     mt19937 mt(randomDevice()); // seed the mt19937 generator using randomDevice
-    uniform_int_distribution<us> randomID(range1, range2);
+    uniform_int_distribution<int> randomID(range1, range2);
     return randomID(mt); // return random number from range1 to range 2
 }
